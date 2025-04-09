3b0951995cd8b63ef057366d81b1ee42dafe9eaf
helloleemo <87022439+helloleemo@users.noreply.github.com>
2025-04-09 17:22:51 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#2F8CF4',
+        'blackColor': '#3D3D3D',
+      },
+      animation: {
+        shimmer: "shimmer 1.5s infinite linear",
+      },
+      keyframes: {
+        shimmer: {
+          "0%": { backgroundPosition: "-200% 0" },
+          "100%": { backgroundPosition: "200% 0" },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
